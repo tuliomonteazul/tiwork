6cedd324c51947e0b1c13aa2710f403c5c1f1737
goesmeira <goesmeira@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-04 19:41:48 +0000

    adicionado classes de negocios e assinaturas de alguns métodos.

---
+++  tiwork/PayLess/principal/persistencia/UsuarioHsql.java
@@ -0,0 +1,25 @@
+package persistencia;
+
+import beans.Usuario;
+
+public class UsuarioHsql implements UsuarioDao {
+
+	@Override
+	public void cadastrarUsuario(Usuario u, String tipo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removerUsuario(Usuario u) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Usuario trazer(String login, String senha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
