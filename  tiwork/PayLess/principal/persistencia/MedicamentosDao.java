d286a85029790c2a7c310c8846b1f2f247e38386
tuliomonteazul <tuliomonteazul@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-04 11:57:53 +0000

    teste

---  tiwork/PayLess/principal/persistencia/MedicamentosDao.java
+++  tiwork/PayLess/principal/persistencia/MedicamentosDao.java
@@ -1,12 +1,14 @@
 package persistencia;
 
+import java.util.Date;
 import java.util.List;
 
 import beans.Medicamentos;
+import beans.Remedio;
 
 public interface MedicamentosDao {
  void cadastrarMedicamentos(Medicamentos m);
  Medicamentos trazer(String nomeMedicamento);
  List<Medicamentos> listar();
- 
+ List<Remedio> listarRemediosPorData(Date dataInicio,Date dataFim);
 }
