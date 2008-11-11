53e67c8b373f9ea88386087487bb19d3910fee3a
tuliomonteazul <tuliomonteazul@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-11 03:41:41 +0000

    removido o metodo cadastarFuncionario e cadastrarFarmaceutico adicionado o metodo cadastrarUsuario

--- trunk/ tiwork/PayLess/principal/persistencia/UsuarioDao.java
+++ trunk/ tiwork/PayLess/principal/persistencia/UsuarioDao.java
@@ -5,9 +5,12 @@ import java.sql.SQLException;
 import beans.Usuario;
 
 public interface UsuarioDao {
- void cadastrarFuncionario(Usuario u)throws SQLException;
- Usuario trazerFarmaceutico(String login)throws SQLException;
- void removerFuncionario(Usuario u)throws SQLException;
- Usuario trazerFuncionario(String login)throws SQLException;
- void cadastrarFarmaceutico(Usuario u)throws SQLException;
+	void cadastrarUsuario(Usuario u) throws SQLException;
+
+	Usuario trazerFarmaceutico(String login) throws SQLException;
+
+	void removerFuncionario(Usuario u) throws SQLException;
+
+	Usuario trazerFuncionario(String login) throws SQLException;
+
 }
