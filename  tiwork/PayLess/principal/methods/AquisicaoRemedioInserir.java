2665b49599267cd388929c69d1abcfec0b8ed3a2
tuliomonteazul <tuliomonteazul@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-11 20:25:34 +0000

    criada as classes de aquisicao e alterado efetuar login

---
+++  tiwork/PayLess/principal/methods/AquisicaoRemedioInserir.java
@@ -0,0 +1,41 @@
+package methods;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import negocio.MedicamentoNegocio;
+import beans.Medicamentos;
+
+public class AquisicaoRemedioInserir implements Method{
+
+	@Override
+	public void doMethod(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		List<Medicamentos> medicamentos = null;
+		MedicamentoNegocio medicamentoNegocio = null;
+		
+		try {
+			RequestDispatcher d = req.getRequestDispatcher("aquisicaoRemedio.jsp");;
+			medicamentoNegocio = new MedicamentoNegocio();
+			medicamentos = medicamentoNegocio.listar();
+			req.setAttribute("medicamentos", medicamentos);
+			d.forward(req, resp);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	
+	
+	}
+
+}
