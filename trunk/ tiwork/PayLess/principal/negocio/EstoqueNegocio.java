c38c7b8c796b1e4573d22ee4830fb40a2a63be82
tuliomonteazul <tuliomonteazul@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-12 03:15:54 +0000

    criada a classe EstoqueNegocio

---
+++ trunk/ tiwork/PayLess/principal/negocio/EstoqueNegocio.java
@@ -0,0 +1,41 @@
+package negocio;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import persistencia.EstoqueDao;
+import persistencia.EstoqueHsql;
+import beans.Estoque;
+import beans.Usuario;
+
+public class EstoqueNegocio{
+	private EstoqueDao estoqueDao;
+	public EstoqueNegocio() throws ClassNotFoundException, SQLException, FileNotFoundException, IOException{
+		estoqueDao =  new EstoqueHsql();
+	}
+	
+	/*
+	 * Metódo inserir um objeto estoque no banco
+	 *
+	 * @param Estoque
+	 * @throws SQLException
+	*/
+	public void inserirEstoque(Estoque estoque)throws SQLException{
+		estoqueDao.inserirEstoque(estoque);
+	}
+
+	/*
+	 * Metódo para retornar uma List com todos o elementos da tabela Estoque
+	 *
+	 * @throws SQLException
+	*/
+	public List<Estoque> listarEstoques()
+			throws SQLException {
+		List<Estoque> estoques = estoqueDao.listarEstoques();
+		return estoques;
+	}
+
+
+}
