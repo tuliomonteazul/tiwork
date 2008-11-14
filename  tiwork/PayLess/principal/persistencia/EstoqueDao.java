5605016cc9303d295f2c5872fad89002793660d0
tuliomonteazul <tuliomonteazul@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-14 03:19:25 +0000

    adicionado listarEstoquesDistinct

---  tiwork/PayLess/principal/persistencia/EstoqueDao.java
+++  tiwork/PayLess/principal/persistencia/EstoqueDao.java
@@ -8,5 +8,6 @@ import beans.Estoque;
 public interface EstoqueDao {
 	void inserirEstoque(Estoque estoque) throws SQLException;
 	List<Estoque> listarEstoques() throws SQLException;
+	List<Estoque> listarEstoquesDistinct() throws SQLException;
 
 }
