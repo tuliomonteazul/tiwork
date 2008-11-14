eaf279afcf12faa5102bdeba18abb62e1b326194
tuliomonteazul <tuliomonteazul@75ee66e0-a557-11dd-9bad-6bc74a82fd44>
2008-11-14 03:20:19 +0000

    criados os methods de efetuar venda

---
+++ trunk/ tiwork/PayLess/principal/methods/EfetuarVendaDetalhesMethod.java
@@ -0,0 +1,67 @@
+package methods;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import negocio.EstoqueNegocio;
+import negocio.MedicamentoNegocio;
+import beans.Estoque;
+import beans.Medicamentos;
+
+public class EfetuarVendaDetalhesMethod implements Method{
+
+	@Override
+	public void doMethod(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		Estoque estoque = null;
+		List<Estoque> estoquesDistinct = null;
+		List<Estoque> estoques = null;
+		EstoqueNegocio estoqueNegocio = null;
+		String nome = req.getParameter("remedio");
+		
+		try {
+			RequestDispatcher d = req.getRequestDispatcher("funcionario/efetuarVenda.jsp");;
+			estoqueNegocio = new EstoqueNegocio();
+			estoquesDistinct = estoqueNegocio.listarEstoquesDistinct();
+			estoques = estoqueNegocio.listarEstoques();
+			for (Estoque e : estoquesDistinct){
+				if (e.getNome().equalsIgnoreCase(nome)){
+					estoque = e;
+				}
+			}
+			if (estoque!=null){
+				for (Estoque e : estoques){
+					if (e.getCod() == estoque.getCod()){
+						estoque.setQuantidade(e.getQuantidade());
+						estoque.setValor(e.getValor());
+					}
+				}
+				for (Estoque ed : estoquesDistinct){
+					for (Estoque e : estoques){
+						if (ed.getCod() == e.getCod()){
+							ed.setQuantidade(ed.getQuantidade()+e.getQuantidade());
+						}
+					}
+				}
+				req.setAttribute("estoque", estoque);
+			}else{
+				req.setAttribute("erro", "ERRO - Nome do remédio inválido.");
+			}
+			req.setAttribute("estoques", estoquesDistinct);
+			d.forward(req, resp);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
